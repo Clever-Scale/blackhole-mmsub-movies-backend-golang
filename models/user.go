65769d201e2280881092990d1a7df925648ef98e
Hein Ko Zin <heinkozin4@gmail.com>
2022-08-19 16:56:57 +0630

    🖥️ wip: test image compression

--- models/user.go
+++ models/user.go
@@ -7,11 +7,12 @@ import (
 )
 
 type User struct {
-	ID        uint           `json:"id" gorm:"primary_key"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email" gorm:"unique_index"`
-	Password  string         `json:"-"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	ID         uint           `json:"id" gorm:"primary_key"`
+	Name       string         `json:"name"`
+	Email      string         `json:"email" gorm:"unique_index"`
+	Password   string         `json:"-"`
+	ProfilePic string         `json:"profile_pic"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
