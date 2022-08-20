cb3d2fa9409d8ca6f42e40a19f974df6db1eca48
Hein Ko Zin <heinkozin4@gmail.com>
2022-08-20 15:24:05 +0630

    ✨feature: tailwind is added

--- blackhole-dashboard/src/main.tsx
+++ blackhole-dashboard/src/main.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "@mui/material";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,8 +7,10 @@ import theme from "./libs/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</StyledEngineProvider>
 	</React.StrictMode>
 );
